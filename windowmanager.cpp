735664cb7316a39eed5c25f85c65252a80ef7aff
Matteo Martincigh <matteo.martincigh@gmail.com>
2016-01-10 13:09:23 +0000

    Added Linux support. Updated version to 2.0.0.

--- windowmanager.cpp
+++ windowmanager.cpp
@@ -1,6 +1,22 @@
 #include "windowmanager.h"
 
-WindowManager::WindowManager()
+WindowManager::WindowManager(QObject *parent) :
+    Base("WM", parent),
+    #if defined(Q_OS_WIN)
+    m_windowFlags(Qt::Window)
+    #else
+    m_windowFlags(Qt::WindowMinimizeButtonHint | Qt::WindowCloseButtonHint)
+    #endif
 {
+    this->debug("Window manager manager created");
+}
 
+WindowManager::~WindowManager()
+{
+    this->debug("Window manager manager created");
+}
+
+Qt::WindowFlags WindowManager::windowFlags() const
+{
+    return m_windowFlags;
 }
--- windowmanager.h
+++ windowmanager.h
@@ -3,10 +3,22 @@
 
 #include <QObject>
 
+#include "base.h"
+
 class WindowManager : public Base
 {
+    Q_OBJECT
+    Q_PROPERTY(Qt::WindowFlags windowFlags READ windowFlags CONSTANT)
+
+private:
+    Qt::WindowFlags m_windowFlags;
+
 public:
-    WindowManager();
+    explicit WindowManager(QObject *parent = NULL);
+    ~WindowManager();
+
+public:
+    Qt::WindowFlags windowFlags() const;
 };
 
 #endif // WINDOWMANAGER_H
